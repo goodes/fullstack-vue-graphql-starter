1db0d36f05bfd92fd7e0ba3d03ef9dc292b4c9e0
Daniel Goodman <daniel.goodman@toganetworks.com>
2019-09-26 12:25:38 +0300

    Section 6: Adding image carousel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,18 +1,17 @@
+
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostsQuery">
-      <template
-        slot-scope="{ result: { loading, error, data, networkStatus } }"
-      >
-        <div v-if="loading">Loading ...</div>
-        <div v-else-if="error">Error {{ error.message }}</div>
-        <div v-else-if="!loading">Network Status: {{ networkStatus }}</div>
-        <ul v-else-if="data" v-for="post in data.getPosts" :key="post._id">
-          <li>{{ post.title }} {{ post.imageUrl }} {{ post.likes }}</li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container text-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ cycle: true }" interval="3000">
+        <v-carousel-item
+          v-for="post in getPosts"
+          :key="post._id"
+          :src="post.imageUrl"
+        >
+          <h1 id="carousel__title">{{ post.title }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
@@ -21,20 +20,32 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "home",
-  data() {
-    return {
-      getPostsQuery: gql`
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
           getPosts {
             _id
             title
             imageUrl
             description
-            likes
           }
         }
       `
-    };
+    }
   }
 };
 </script>
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
