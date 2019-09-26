601dc342482ad9aa8569647afe8d4331173eac5a
Daniel Goodman <daniel.goodman@toganetworks.com>
2019-09-26 10:23:42 +0300

    Section 5: Add side navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,55 @@
 <template>
-  <v-app>
+  <v-app style="background: #E3E3EE">
+
+    <!-- Side NavBar -->
+    <v-navigation-drawer
+      app
+      temporary
+      fixed
+      v-model="sideNav"
+    >
+      <v-app-bar
+        color="accent"
+        dark
+        flat
+      >
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link
+          to="/"
+          tag="span"
+          style="cursor: pointer"
+        >
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-app-bar>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          ripple
+          v-for="item in sideNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-item-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            {{ item.title}}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Horizonatal NavBar -->
     <v-app-bar
       fixed
       color="primary"
       dark
     >
       <!-- App Title -->
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link
           to="/"
@@ -58,6 +101,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
@@ -65,6 +113,18 @@ export default {
         { icon: "mdi-lock-open", title: "Sign In ", link: "/signin" },
         { icon: "mdi-pencil", title: "Sign Up ", link: "/signup" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "mdi-chat", title: "Posts", link: "/posts" },
+        { icon: "mdi-lock-open", title: "Sign In ", link: "/signin" },
+        { icon: "mdi-pencil", title: "Sign Up ", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };
