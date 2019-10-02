85a438746b90630fe93c4beee59c5aad677ce8ac
Daniel Goodman <daniel.goodman@toganetworks.com>
2019-10-02 11:10:50 +0300

    Section 8: Add password hash and avatar [34]

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const md5 = require('md5');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -13,4 +15,25 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.pre('save', function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+// Hash password so that if someone has access to DB it cannot be seen
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
+});
 module.exports = mongoose.model('User', UserSchema);
